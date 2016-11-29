f3cbdfb5a3dbed062c6b34f0cac4cf662af91f02
Your Name <kelvin.lightner@gmail.com>
2016-11-28 20:35:55 -0800

    Setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(title: "Build Muscle")
+Category.create(title: "Weight Loss")
+Category.create(title: "Full Transformation")
+
+@levels = ["Advanced", "Intermediate", "Beginner"]
+
+@categories = Category.all
+
+
+20.times do Article.create(
+	name: "12-Week Daily Bulking Trainer",
+	level: @levels.sample,
+	duration: "12 Weeks",
+	category_id: @categories.sample.id
+	)
+end
